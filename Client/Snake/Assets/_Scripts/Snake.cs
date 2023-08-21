0d7a202ecaaf386e0968bd997c757847af172b8f
Alexander <102901721+Labu1ay@users.noreply.github.com>
2023-08-21 23:58:54 +0300

    Tail

---
+++ Client/Snake/Assets/_Scripts/Snake.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Snake : MonoBehaviour {
+    [SerializeField] private Transform _head;
+    [SerializeField] private Transform _directionPoint;
+    [SerializeField] private float _speed = 2f;
+    [SerializeField] private float _rotateSpeed = 90f;
+    private void Update() {
+        Rotate();
+        Move();
+    }
+    private void Rotate(){
+        Quaternion targetRotation = Quaternion.LookRotation(_targetDirection);
+        _head.rotation = Quaternion.RotateTowards(_head.rotation, targetRotation, Time.deltaTime * _rotateSpeed);
+        /* float diffY = _directionPoint.eulerAngles.y - _head.eulerAngles.y;
+
+        if(diffY > 180) diffY = (diffY - 180) * -1;
+        else if(diffY < -180) diffY = (diffY + 180) * -1;
+
+        float maxAngle = Time.deltaTime * _rotateSpeed;
+        float rotateY = Mathf.Clamp(diffY, -maxAngle, maxAngle);
+
+        _head.Rotate(0, rotateY, 0); */
+    }
+    private void Move(){
+        transform.position += _head.forward * Time.deltaTime * _speed;
+    }
+    private Vector3 _targetDirection = Vector3.zero;
+    public void LookAt(Vector3 cursorPosition){
+        _targetDirection = cursorPosition - _head.position;
+        //_directionPoint.LookAt(cursorPosition);
+    }
+}
